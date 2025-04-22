aea915bad594bd5e340f50a8c64747d405e9cecb
Mads Christensen <mads@kvalifik.dk>
2025-04-22 23:01:36 +0200

    move prettier add bullmodule to app.module

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { BullModule } from '@nestjs/bullmq'
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
