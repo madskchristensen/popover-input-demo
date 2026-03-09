f8c4dae2071da1dc3c15e1977e61963b95a79bde
madskchristensen <m.kjaergaard.christensen@gmail.com>
2026-03-09 16:31:36 +0100

    scaffold orval configure auto-annotation for dtos,entities

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+
+  app.enableCors({
+    origin: process.env.NEXT_URL ?? 'http://localhost:3000',
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Applications API')
+    .setDescription('API for managing job applications')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3001)
 }
+
 bootstrap()
---
+++ nest/src/scripts/swagger-export.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { writeFileSync } from 'fs'
+import { resolve } from 'path'
+import { AppModule } from 'src/app.module'
+
+async function exportSwagger() {
+  const app = await NestFactory.create(AppModule, { logger: false })
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Applications API')
+    .setDescription('API for managing job applications')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  const outputPath = resolve(process.cwd(), 'swagger.json')
+  writeFileSync(outputPath, JSON.stringify(document, null, 2), 'utf8')
+  console.log(`Swagger JSON written to ${outputPath}`)
+
+  await app.close()
+  process.exit(0)
+}
+
+exportSwagger()
