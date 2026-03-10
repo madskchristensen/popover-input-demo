49b4f3be299f85a6c540bb2aac89fb40458e7744
madskchristensen <m.kjaergaard.christensen@gmail.com>
2026-03-10 14:29:41 +0100

    implement table data serialization

--- next/components/JobApplicationTable/hooks/useSerializeJobApplicationTableData.ts
+++ next/components/JobApplicationTable/hooks/useSerializeJobApplicationTableData.ts
@@ -3,35 +3,38 @@ import { DateTime } from 'luxon'
 import { JobApplication } from '@/orval/generated/models'
 import { JobApplicationTableItem } from '../types/TableItem'
 
-// TODO: Adjust namings, data etc.
 export const useSerializeJobApplicationTableData = (
-  data: JobApplication[],
+  jobApplications: JobApplication[],
 ): JobApplicationTableItem[] => {
   const formatDate = (isoDate: string) =>
     DateTime.fromISO(isoDate).toFormat('dd//MM/yy')
 
   return useMemo(() => {
-    return data.map((submission) => {
-      const contractSignedByBothParties = Boolean(
-        submission.contractSignedByAdminAt,
-      )
+    return jobApplications.map((application) => {
+      const {
+        country,
+        createdAt,
+        email,
+        firstName,
+        id,
+        jobRole,
+        jobRoleId,
+        lastName,
+        status,
+        updatedAt,
+      } = application
 
       return {
-        id: submission.id,
-        car: `${submission.carBrand} ${submission.carModel} ${submission.carVersion}`,
-        owner: submission.user?.fullName ?? 'User fullname missing!',
-        status: 'stepper status was here', // TODO: keep? was: getStepperStatus('internal', submission)?.title
-        submittedDate: formatDate(submission.dateSubmitted),
-        carNumber: submission.carNumber ?? '-',
-        country: submission.countryCode,
-        documentsRequested: submission.documentsRequested ?? '',
-        photosRequested: submission.photosRequested,
-        isRecurringSeller: Boolean(submission.user?.isRecurringSeller),
-        primaryPhotoUrl: submission.primaryPhotoUrl,
-        resubmissionCount: submission.resubmissionCount,
-        contractSignedByBothParties,
-        isCarPaid: submission.car.paymentStatus === 'PAID',
-      }
+        id,
+        firstName,
+        lastName,
+        email,
+        status,
+        country,
+        createdAt: formatDate(createdAt),
+        jobRole: jobRole.title, // TODO: Re-check this. How do i want to serialize?
+        jobCategory: jobRole.jobCategory.name, // TODO: Re-check this. How do i want to serialize?
+      } satisfies JobApplicationTableItem
     })
-  }, [data])
+  }, [jobApplications])
 }
