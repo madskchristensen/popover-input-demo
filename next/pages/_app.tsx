24f3ba1cfdaca219d13634aa1c2ab8a7f7e352e2
madskchristensen <m.kjaergaard.christensen@gmail.com>
2026-03-10 16:18:43 +0100

    fix missing App wrapper components

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import type { AppProps } from 'next/app'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  )
 }
