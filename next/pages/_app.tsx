749ebbef995bc81427daa5b9ffe1ad43af748578
madskchristensen <m.kjaergaard.christensen@gmail.com>
2026-03-11 12:56:17 +0100

    move react query dev tools position

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
+        <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
         <Component {...pageProps} />
       </QueryClientProvider>
     </ChakraProvider>
