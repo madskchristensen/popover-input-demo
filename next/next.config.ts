ea26f27aa3e87853ffbbbf970e8e238c46a17146
madskchristensen <m.kjaergaard.christensen@gmail.com>
2026-03-12 19:57:14 +0100

    remove webpack config

--- next/next.config.ts
+++ next/next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  webpack: (config) => {
-    // Helps hot reloading when served in Docker on Windows machines.
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
 }
 
 export default nextConfig
